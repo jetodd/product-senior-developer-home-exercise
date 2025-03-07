4aa72291f7408c13701b95782ab711da02e43308
Jess Todd <jessstodd@gmail.com>
2025-03-08 08:07:06 +1300

    Workaround bug in exercise

--- UKParliament.CodeTest.Web/ClientApp/aspnetcore-https.js
+++ UKParliament.CodeTest.Web/ClientApp/aspnetcore-https.js
@@ -8,6 +8,9 @@ const baseFolder =
     ? `${process.env.APPDATA}/ASP.NET/https`
     : `${process.env.HOME}/.aspnet/https`;
 
+// https://stackoverflow.com/questions/79148729/asp-net-core-development-certificates-error-exporting-the-https-developer-cert
+fs.mkdirSync(baseFolder, { recursive: true });
+
 const certificateArg = process.argv.map(arg => arg.match(/--name=(?<value>.+)/i)).filter(Boolean)[0];
 const certificateName = certificateArg ? certificateArg.groups.value : process.env.npm_package_name;
 
