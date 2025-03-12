98cbe6b4e5fcc160ac44230f38a36f9ed0eec7a9
Jess Todd <jessstodd@gmail.com>
2025-03-12 12:31:45 +0000

    added department controller tests

--- UKParliament.CodeTest.Tests/ControllerTests/DepartmentControllerTests.cs
+++ UKParliament.CodeTest.Tests/ControllerTests/DepartmentControllerTests.cs
@@ -1,9 +1,34 @@
 using Xunit;
 using UKParliament.CodeTest.Data;
-using EntityFrameworkCore.Testing.Moq;
+using Moq;
+using UKParliament.CodeTest.Services;
+using UKParliament.CodeTest.Web.Controllers;
+using Microsoft.AspNetCore.Mvc;
 
 namespace UKParliament.CodeTest.Tests.ControllerTests;
 
 public class DepartmentControllerTests
 {
+     private readonly List<Department> mockDepartments = new List<Department> {
+        new Department { Id = 1, Name = "Sales" },
+        new Department { Id = 2, Name = "Marketing" },
+        new Department { Id = 3, Name = "Finance" },
+        new Department { Id = 4, Name = "HR" }
+    };
+
+    [Fact]
+    public void GetDepartments_ReturnsListOfDepartments() {
+        var mockDepartmentService = new Mock<IDepartmentService>();
+        mockDepartmentService.Setup(d => d.GetDepartments()).Returns(mockDepartments);
+
+        var controller = new DepartmentController(mockDepartmentService.Object);
+
+        var result = controller.GetDepartments();
+        var okResult = result.Result as OkObjectResult;
+
+        mockDepartmentService.Verify(r => r.GetDepartments(), Times.Once);
+
+        Assert.NotNull(okResult);
+        Assert.Equal(200, okResult.StatusCode);
+    }
 }
